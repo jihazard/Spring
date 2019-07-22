e821f0bf5f33aef3391df8ea67bc91748fe472d0
jihazard <yoonjh238@gmail.com>
2019-07-22 10:40:31 +0900

    jpa 양반향 맵핑

--- jpatest/src/main/java/com/spring/jpa/Main.java
+++ jpatest/src/main/java/com/spring/jpa/Main.java
@@ -5,6 +5,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import java.util.Date;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -28,13 +29,22 @@ public class Main {
         member.setTeam(team);
         em.persist(member);
 
+        Member member1 = new Member();
+        member1.setAge(36);
+        member1.setName("윤지환2");
+        member1.setRole(MemberType.USER);
+        member1.setRegDate(new Date());
+        member1.setTeam(team);
+        em.persist(member1);
 
         Member findMember = em.find(Member.class, member.getId());
 
-
         System.out.println(findMember.toString());
         Team team1 = em.find(Team.class, member.getId());
-        System.out.println(team1.toString());
+        System.out.println(findMember.getTeam());
+        List<Member> members = team1.getMembers();
+        System.out.println("사이즈 : " + members.size());
+        for(Member vo : members) System.out.println(vo.toString());
 
         tx.commit();
     }catch (Exception e){
